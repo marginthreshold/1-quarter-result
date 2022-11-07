e5cf40757c845ba345d5a99b8f5a3e627bd5080c
Lavanov <alavanov@rencap.com>
2022-11-07 11:07:22 +0300

    Created method Extracted less than 4 Letters elements from string array

---
+++ Extract 3-letters elements from StringArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using System.Linq;
+Clear();
+
+WriteLine("Введите строки в массив через проблел: ");
+string[] userarray = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+string[] clearedArray = ExtractLess4LettersElements(userarray);
+WriteLine($"[{String.Join(" ", clearedArray)}]");
+
+
+
+string[] ExtractLess4LettersElements(string[] array)
+{
+    string[] newarray = { };
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i].Length < 4)
+        {
+            newarray = newarray.Append(array[i]).ToArray();
+        }
+
+
+    }
+
+
+    return newarray;
+}
